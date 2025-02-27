20374bed5e1fc29d064a9cbd8c4ca5dc46f308c4
supertypo <suprtypo@pm.me>
2025-02-27 09:32:44 +0100

    Updated is-synced port selection

--- is-synced-grpc.sh
+++ is-synced-grpc.sh
@@ -10,10 +10,15 @@ elif [ -z "$4" -o "$4" = "0" ]; then
   HOST_PORT=16110 # Default to mainnet
   case "$HAPROXY_PROXY_NAME" in
     *test*)
-      HOST_PORT=16210 # Assume testnet for backends with the name test in them
+      HOST_PORT=16210 # Assume testnet-10 for backends with test in them
+      ;;
+    *tn10*)
+      HOST_PORT=16210 # Assume testnet-10 for backends with tn10 in them
+      ;;
+    *tn11*)
+      HOST_PORT=16310 # Assume testnet-11 for backends with tn11 in them
       ;;
   esac
 fi
 
 ${PWD}/kaspactl -a -s $HOST_IP:$HOST_PORT -t $TIMEOUT_SEC GetInfo 2>/dev/null | /bin/grep -qE '"isSynced": *true'
-
--- is-synced-wrpc.sh
+++ is-synced-wrpc.sh
@@ -10,10 +10,15 @@ elif [ -z "$4" -o "$4" = "0" ]; then
   HOST_PORT=17110 # Default to mainnet
   case "$HAPROXY_PROXY_NAME" in
     *test*)
-      HOST_PORT=17210 # Assume testnet for backends with the name test in them
+      HOST_PORT=17210 # Assume testnet-10 for backends with test in them
+      ;;
+    *tn10*)
+      HOST_PORT=17210 # Assume testnet-10 for backends with tn10 in them
+      ;;
+    *tn11*)
+      HOST_PORT=17310 # Assume testnet-11 for backends with tn11 in them
       ;;
   esac
 fi
 
 ${PWD}/kcheck --timeout=${TIMEOUT_SEC}000 ws://$HOST_IP:$HOST_PORT
-
